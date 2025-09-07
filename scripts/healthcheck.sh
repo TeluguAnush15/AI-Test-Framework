2dc5b34aea46ff23bd5fe7d2d987088041d8851e
TeluguAnush15 <92114054+TeluguAnush15@users.noreply.github.com>
2025-09-07 23:12:06 +0530

    Update healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+echo "🩺 Running health check..."
+
+# Simulate a health check by checking if report folder exists
+if [ -d /tmp/prod-allure ]; then
+  echo "✅ Health check passed!"
+  exit 0
+else
+  echo "❌ Health check failed!"
+  exit 1
+fi
 
